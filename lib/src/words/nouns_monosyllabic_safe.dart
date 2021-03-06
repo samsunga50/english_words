5a675cf97a9955851326164f20791e79333730fc
samsunga50 <61355569+samsunga50@users.noreply.github.com>
2021-03-07 00:22:48 +0400

    Update nouns_monosyllabic_safe.dart

--- lib/src/words/nouns_monosyllabic_safe.dart
+++ lib/src/words/nouns_monosyllabic_safe.dart
@@ -2,6 +2,19 @@
 ///
 /// The list is sorted in order of frequency of use, descending.
 const List<String> nounsMonosyllabicSafe = [
-  'gros',
-  'caca',
+'caca',
+'pt',
+'fesse',
+'trou',
+'graine',
+'bobok',
+'gopia',
+'gros',
+'bourik',
+'sunuk',
+'imbecile',
+'zozo',
+'tipti',
+'laguel',
+'boufon',
 ];
