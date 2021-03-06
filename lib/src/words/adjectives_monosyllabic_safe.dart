fd037573bce17ea30f565b4411ef39e1e1eea46e
samsunga50 <61355569+samsunga50@users.noreply.github.com>
2021-03-07 00:22:00 +0400

    Update adjectives_monosyllabic_safe.dart

--- lib/src/words/adjectives_monosyllabic_safe.dart
+++ lib/src/words/adjectives_monosyllabic_safe.dart
@@ -2,6 +2,19 @@
 ///
 /// The list is sorted in order of frequency of use, descending.
 const List<String> adjectivesMonosyllabicSafe = [
-  'caca',
-  'fesse',
+'caca',
+'pt',
+'fesse',
+'trou',
+'graine',
+'bobok',
+'gopia',
+'gros',
+'bourik',
+'sunuk',
+'imbecile',
+'zozo',
+'tipti',
+'laguel',
+'boufon',
 ];
